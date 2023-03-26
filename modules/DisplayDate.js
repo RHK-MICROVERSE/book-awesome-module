6d5d8d1d88868304c0b8954855e84d8bb54d246a
Rassel Hassan Kadir <9rhktrading@gmail.com>
2023-03-27 00:20:14 +0600

    Implementation of ES6 and Modules for JavaScript

--- modules/DisplayDate.js
+++ modules/DisplayDate.js
@@ -1,6 +1,8 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-//Sun Mar 26 2023 22:37:29 GMT+0600 (Bangladesh Standard Time)
+// Sun Mar 26 2023 22:37:29 GMT+0600 (Bangladesh Standard Time)
+
+/* eslint-disable no-unused-vars */
 let displayDateTime;
 export default displayDateTime = () => {
   const dt = DateTime.local(new Date());
@@ -9,6 +11,4 @@ export default displayDateTime = () => {
   const hourZoneTime = dt.toLocaleString(DateTime.TIME_24_WITH_LONG_OFFSET);
   const result = `${justhour} ${test.slice(-5)} (${hourZoneTime.slice(-25)})`;
   return result;
-}
-
-// export { displayDateTime };
+};
--- modules/functions.js
+++ modules/functions.js
@@ -1,23 +1,22 @@
-let onlyDisplayBook = () => {
+const onlyDisplayBook = () => {
   document.getElementById('display-books').style.display = 'unset';
   document.getElementById('separation-book-form').style.display = 'none';
   document.getElementById('input-book').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-}
+};
 
-let onlyDisplayInputBook = () => {
+const onlyDisplayInputBook = () => {
   document.getElementById('input-book').style.display = 'unset';
   document.getElementById('display-books').style.display = 'none';
   document.getElementById('separation-book-form').style.display = 'none';
   document.getElementById('contact').style.display = 'none';
-}
+};
 
-let onlyDisplayContact = () => {
+const onlyDisplayContact = () => {
   document.getElementById('contact').style.display = 'unset';
   document.getElementById('display-books').style.display = 'none';
   document.getElementById('separation-book-form').style.display = 'none';
   document.getElementById('input-book').style.display = 'none';
-}
-
+};
 
 export { onlyDisplayBook, onlyDisplayInputBook, onlyDisplayContact };
